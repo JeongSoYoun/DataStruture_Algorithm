aef61e083fe43a793b694ff959d689fbf377fc6a
JeongSoYoun <socozy7210@gmail.com>
2021-04-26 01:45:39 +0900

    insertion sort

--- Algorithm/insertionSort.cpp
+++ Algorithm/insertionSort.cpp
@@ -6,9 +6,36 @@
 
 using namespace std;
 
+void insertion_sort(int* array ,int num);
+
 int main() {
 
-    
+    int needToBeSorted[] = {6,4,3,2,1,5,7};
+    int arrayCount = sizeof(needToBeSorted)/sizeof(int);
 
+    insertion_sort(needToBeSorted,arrayCount);
+
+    for(int i = 0; i < arrayCount; i++) {
+
+        cout << needToBeSorted[i] << " ";
+    }
+    
     return 0;
+}
+
+void insertion_sort(int* array ,int num) {
+
+    for(int j = 1; j < num; j++) {
+
+        int key = *(array+j);
+        int i = j-1;
+
+        while (i > -1 && *(array+i) > key) {
+
+            *(array+(i+1)) = *(array+i);
+            i = i-1;
+        }
+
+        *(array+(i+1)) = key;
+    }
 }
